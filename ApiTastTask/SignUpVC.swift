93a604b9dd2abeaa455b9c96cf4f10782e18db82
Nikita Nechyporenko <22tictac22@ukr.net>
2018-09-24 13:23:53 +0300

    pod install and setup SignUpViewController

--- ApiTastTask/SignUpVC.swift
+++ ApiTastTask/SignUpVC.swift
@@ -7,29 +7,86 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
+import SDWebImage
 
 class SignUpVC: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    
+    var name = ""
+    var email = ""
+    var password = ""
+    
+    struct SuccessData: Any {
+        var success: Bool?
+        var data: PulledData?
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    struct PulledData: Any {
+        var uid : Int
+        var name : String
+        var email : String
+        var access_token : String
+        var role : Int
+        var status : Int
+        var created_at : Int
+        var updated_at : Int
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        name = nameTextField.text!
+        email = emailTextField.text!
+        password = passwordTextField.text!
     }
-    */
-
+    
+    func sendRequestRequest(name: String, email: String, password: String) {
+        /**
+         Request
+         post http://apiecho.cf/api/signup/
+         */
+        
+        // Add Headers
+        let headers = [
+            "Content-Type":"application/json; charset=utf-8",
+            ]
+        
+        // JSON Body
+        let body: [String : Any] = [
+            "name": name,
+            "email": email,
+            "password": password
+        ]
+        
+        // Fetch Request
+        Alamofire.request("http://apiecho.cf/api/signup/", method: .post, parameters: body, encoding: JSONEncoding.default, headers: headers)
+            .validate(statusCode: 200..<300)
+            .responseJSON { response in
+                if (response.result.error == nil) {
+                    debugPrint("HTTP Response Body: \(response.description)")
+                    //                    let swiftyJsonVar = JSON(response.result.value!) as? [String: Any]
+                    //                    print("swiftyJsonVar = \(swiftyJsonVar)")
+                    
+                }
+                else {
+                    debugPrint("HTTP Request failed: \(response.result.error)")
+                }
+        }
+    }
+    
+    @IBAction func signUpButton(_ sender: Any) {
+        
+        name = nameTextField.text!
+        print("name - \(nameTextField.text)")
+        email = emailTextField.text!
+        print("email - \(emailTextField.text)")
+        password = passwordTextField.text!
+        print("password - \(passwordTextField.text)")
+        sendRequestRequest(name: name, email: email, password: password)
+        
+    }
 }
---
+++ Pods/Target Support Files/Pods-ApiTastTask/Pods-ApiTastTask-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ApiTastTask : NSObject
+@end
+@implementation PodsDummy_Pods_ApiTastTask
+@end
