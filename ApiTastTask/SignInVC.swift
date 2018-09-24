b9ad2c22938f4ea0229083da36c797a3c90e19d9
Nikita Nechyporenko <22tictac22@ukr.net>
2018-09-24 14:20:34 +0300

    setup SignInViewController

--- ApiTastTask/SignInVC.swift
+++ ApiTastTask/SignInVC.swift
@@ -7,15 +7,42 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
 class SignInVC: UIViewController {
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    var email = ""
+    var password = ""
+    
+    struct SuccessData: Any {
+        var success: Bool?
+        var data: PulledData?
+    }
+    struct PulledData: Any {
+        var uid : Int
+        var name : String
+        var email : String
+        var access_token : String
+        var role : Int
+        var status : Int
+        var created_at : Int
+        var updated_at : Int
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        email = emailTextField.text!
+        password = passwordTextField.text!
         
     }
-    
-    
-}
 
+    
+    @IBAction func buttonSignIn(_ sender: Any) {
+        
+}
+}
